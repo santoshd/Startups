7c13b346350cee8d2f7d669e611ac56f9b74ece8
Santosh Dawara <486603+santoshd@users.noreply.github.com>
2025-01-30 17:16:41 +0530

    Quartz sync: Jan 30, 2025, 5:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "1by2.com/startups",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
